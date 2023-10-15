ad59c9fdd703644a922591238945629a616e365a
Yevhen Chementsov <yevhenchementsov@gmail.com>
2023-10-15 19:45:41 +0300

    Update jsconfig, vite.config & README files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   base: '/vite-react-template/',
   plugins: [react(), svgr()],
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: {
+      components: '/src/components',
+      constants: '/src/constants',
+      /*
+        icons: '/src/icons', // <---  add necessary folders here for absolute path
+       */
+    }
   },
 });
